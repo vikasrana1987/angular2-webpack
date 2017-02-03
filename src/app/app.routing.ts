1c3c7c40aef3500397dd0ef6b39bf18c5c0fc619
Vikas Rana <vikas.rana@cloudsmartz.net>
2017-02-03 15:51:12 +0530

    Spinner added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -50,7 +50,7 @@ const routes: Routes = [
       }
     }
   },
-  { path: '**', redirectTo: ''}
+  { path: '**', redirectTo: 'dashboard'}
 ];
 
 export const routing = RouterModule.forRoot(routes);
